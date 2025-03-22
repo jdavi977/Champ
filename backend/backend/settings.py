4c0ad82eb05cfc90f0ed81edb6ee0e441d1ffbe7
acenyioko <aenyioko090@gmail.com>
2025-03-22 15:17:03 -0500

    Added mariadb database into django settigns

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -97,8 +97,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'performance.db',
+        'USER': 'champion',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
