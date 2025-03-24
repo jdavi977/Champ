2ad3d3c28ba8edc07b945eb9d9fbd49c631ac0a8
John <johnlowendavid@yahoo.ca>
2025-03-24 17:05:14 -0600

    added docker

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -98,11 +98,11 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'performance.db',
-        'USER': 'champion',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT', 3306),
     }
 }
 
