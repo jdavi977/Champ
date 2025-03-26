2362e8214071cbce6fcf5f0a68e281cb9ae381d5
John <johnlowendavid@yahoo.ca>
2025-03-25 23:50:38 -0600

    created three screen tabs (home, metrics, profile)

--- frontend/App.js
+++ frontend/App.js
@@ -1,21 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Text, View } from 'react-native';
 
-export default function App() {
+// Dummy screens
+function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Champ</Text>
-      <Text>Test</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function Metrics() {
+  return (
+    <View>
+      <Text>Metrics</Text>
+    </View>
+  )
+}
+
+function ProfileScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Profile</Text>
+    </View>
+  );
+}
+
+// Create bottom tab navigator
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Metrics" component={Metrics} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
