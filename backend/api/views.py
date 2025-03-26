bbca49ab41ae0d9297b71e5f94bb21ddde700619
John <johnlowendavid@yahoo.ca>
2025-03-25 21:44:21 -0600

    Created the serializer, view, and urls for the UserProfile

--- backend/api/views.py
+++ backend/api/views.py
@@ -3,9 +3,16 @@ from django.contrib.auth.models import User
 from rest_framework import generics
 from .serializers import UserSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from .models import UserProfile
+from .serializers import UserProfileSerializer
 
 # Create your views here.
 class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [AllowAny]
+    
+class UserProfileView(generics.ListCreateAPIView):
+    queryset = UserProfile.objects.all()
+    serializer_class = UserProfileSerializer
     permission_classes = [AllowAny]
