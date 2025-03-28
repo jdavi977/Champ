d58f719956101b6ea860031cc38fd7fd57c3f976
John <johnlowendavid@yahoo.ca>
2025-03-27 21:39:20 -0600

    Finished creating navigation bar

--- frontend/App.js
+++ frontend/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
 // Dummy screens
 function HomeScreen() {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={styles.icon}>
       <Text>Home</Text>
     </View>
   );
@@ -14,7 +14,7 @@ function HomeScreen() {
 
 function Metrics() {
   return (
-    <View>
+    <View style={styles.icon}>
       <Text>Metrics</Text>
     </View>
   )
@@ -22,7 +22,7 @@ function Metrics() {
 
 function ProfileScreen() {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={styles.icon}>
       <Text>Profile</Text>
     </View>
   );
@@ -33,8 +33,8 @@ const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator>
+    <NavigationContainer style={styles.iconContainer}>
+      <Tab.Navigator style={styles.iconContainer}>
         <Tab.Screen name="Home" component={HomeScreen} />
         <Tab.Screen name="Metrics" component={Metrics} />
         <Tab.Screen name="Profile" component={ProfileScreen} />
@@ -42,3 +42,12 @@ export default function App() {
     </NavigationContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  icon: {
+
+  },
+  iconContainer: {
+    backgroundColor: "black"
+  }
+})
