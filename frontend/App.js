ea5f7cfa5ce927c5d41a4668ad54d7fecfe09810
John <johnlowendavid@yahoo.ca>
2025-03-27 22:10:27 -0600

    Set up Messages screen component

--- frontend/App.js
+++ frontend/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Text, View } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import MessagesScreen from './screens/MessagesScreen';
 
 // Dummy screens
 function HomeScreen() {
@@ -28,8 +30,24 @@ function ProfileScreen() {
   );
 }
 
+function MessagesStackScreen() {
+  return (
+    <MessagesStack.Navigator>
+      <MessagesStack.Screen 
+        name="Messages"
+        component={MessagesScreen}
+        options= {{
+          headerShown: false
+        }}
+      />
+    </MessagesStack.Navigator>
+  )
+}
+
 // Create bottom tab navigator
 const Tab = createBottomTabNavigator();
+const MessagesStack = createNativeStackNavigator();
+
 
 export default function App() {
   return (
@@ -38,6 +56,7 @@ export default function App() {
         <Tab.Screen name="Home" component={HomeScreen} />
         <Tab.Screen name="Metrics" component={Metrics} />
         <Tab.Screen name="Profile" component={ProfileScreen} />
+        <Tab.Screen name="Messages" component={MessagesStackScreen} />
       </Tab.Navigator>
     </NavigationContainer>
   );
---
+++ frontend/screens/MessagesScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+function MesssagesScreen() {
+    
+
+    return (
+        <View>
+            <Text>hello</Text>
+        </View>
+    );
+} 
+
+export default MesssagesScreen;
